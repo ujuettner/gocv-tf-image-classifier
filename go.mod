ac073776bd974072fb837a6267f27fed86dbfe15
Udo Juettner <udo.juettner@gmail.com>
2021-01-11 01:05:39 +0100

    Add flag handling

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/ujuettner/gocv-tf-image-classifier
 
 go 1.15
+
+require (
+	github.com/spf13/pflag v1.0.5
+	gocv.io/x/gocv v0.25.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	flag "github.com/spf13/pflag"
+	"gocv.io/x/gocv"
+)
 
 func main() {
+	var modelFile *string = flag.StringP("model", "m", "", "TensorFlow model file")
+	var descFile *string = flag.StringP("desc", "d", "", "description file containing the model's labels")
+	var imageFile *string = flag.StringP("image", "i", "", "image file")
+	flag.Parse()
+
+	// check for required flags
+	if *modelFile == "" {
+		fmt.Printf("No model file given!")
+		os.Exit(2)
+	}
+	if *descFile == "" {
+		fmt.Println("No description file given!")
+		os.Exit(2)
+	}
+	if *imageFile == "" {
+		fmt.Println("No image file given!")
+		os.Exit(2)
+	}
+
+	image := gocv.IMRead(*imageFile, gocv.IMReadColor)
+	if image.Empty() {
+		fmt.Printf("Error reading image from: %v\n", *imageFile)
+		return
+	}
+	defer image.Close()
+
 	fmt.Println("TBD!!!")
 }
